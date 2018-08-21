d46066c6f86aef6b1fa85b65bc56be9fd3e045d0
fongvang <fvang1020@gmail.com>
2018-08-21 14:27:05 -0700

    initial commit with changes to home, routes, models, and api

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/public/dist/public")));
 
 router(app);
-app.all("*", (req,res)=>res.sendFile(path.join(__dirname, "/public/dist/public/index.html")));
+app.all("**", (req,res)=>res.sendFile(path.join(__dirname + "/public/dist/public/index.html")));
+
 
 app.listen(8000, ()=>console.log('localhost listing 8000!!'));
--- server/models.js
+++ server/models.js
@@ -1,14 +1,25 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/blog", { useNewUrlParser: true }, (errs)=>console.log(errs||'Run! DB Run!'));
+var mongoose_email = require('mongoose-type-email');
 
 const PostSchema = new mongoose.Schema({
+	email: {
+		type: mongoose_email,
+	},
+	name: {
+		type: String,
+		minlength: [2, "Be at least 2 letters"],
+		required: true,
+	},
 	title: {
 		type: String,
-		minlength: [3, 'title must be at least 3 characters']
+		minlength: [3, 'title must be at least 3 characters'],
+		required: true,
 	},
 	description: {
 		type: String,
-		minlength: [3, 'comments must be at least 3 characters']
+		minlength: [3, 'comments must be at least 3 characters'],
+		required: true,
 	},
 	comments: [{
 		name: {
@@ -20,7 +31,6 @@ const PostSchema = new mongoose.Schema({
 			
 			minlength: [3, 'comment must be at least 3 characters']
 		},
-		
 	}]
 }, {timestamps: true})
 
