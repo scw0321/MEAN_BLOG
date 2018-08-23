99d00e46e51a496534b997e84c685255c41416ef
fongvang <fvang1020@gmail.com>
2018-08-23 16:00:22 -0700

    changes to the show componenets

--- public/src/app/show/show.component.ts
+++ public/src/app/show/show.component.ts
@@ -8,25 +8,54 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./show.component.css']
 })
 export class ShowComponent implements OnInit {
-
+//  General ID
+  id = function (){
+    let observable = this._route.paramMap;
+    observable.subscribe(params=>this.getInfo(params.get('id')));
+  }
+//  Retrieved data saved in _post variable 
   _post: any = {
-      title: "",
-      description: "",
-      answer: []
-      
-    };
+    title: "",
+    description: "",
+    answer: []
+  };
+// answer constructor for http patch 
+  answer: any;
+  errs : any;
+
+
   constructor(private _http: HttpService, private _router: Router, private _route: ActivatedRoute) { }
 
   ngOnInit() {
-  	let robs = this._route.paramMap;
-  	robs.subscribe(params=>this.getInfo(params.get('id')));
+  	let observable = this._route.paramMap;
+    observable.subscribe(params=>this.getInfo(params.get('id')));
+
+    this.answer = "";
   }
 
-  getInfo(id)
-  {
-  	let obs = this._http.getOne(id);
-  	obs.subscribe((data: any[])=>this._post = data);
-  	console.log(['data']);
+  getInfo(id){
+    let obs = this._http.getOne(id);
+    obs.subscribe((data: any[])=>this._post = data);
+    console.log(['data']);  
   }
+
+  addAnswer(){
+    console.log("in the add answer function")
+    let obs = this._http.addAnswer(this.id, this.answer)
+    obs.subscribe(data => {
+      // if('errors' in data){
+      //   this.errs = [];
+      //   for(let x in data['errors']){
+      //     this.errs.push(data['errors'][x]['message'])
+      //   }
+      //   console.log("There were errors!", this.errs);
+      // }else{
+        this.answer = data;
+      // }
+    })
+  }
+
+
+
 
 }
