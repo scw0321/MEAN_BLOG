aee145225a28e94fe20ea6090f80d5fd987f7f43
fongvang <fvang1020@gmail.com>
2018-08-22 18:15:20 -0700

    new changes from cindy + fong's changes to HTML

--- server.js
+++ server.js
@@ -19,19 +19,4 @@ app.use(express.static(path.join(__dirname, "/public/dist/public")));
 router(app);
 app.all("**", (req,res)=>res.sendFile(path.join(__dirname + "/public/dist/public/index.html")));
 
-
-// app.post('/sessions', (req, res) => {
-//     console.log(" req.body: ", req.body);
-//     User.findOne({email:req.body.email, password: req.body.password}, (err, user) => {
-//         if (err) {
-//             // Code...
-//         }
-//         else {
-//             // Code...
-//     		req.session.user_id = user._id;
-// 		req.session.email = user.email;
-//         }
-//     })
-// })
-
 app.listen(8000, ()=>console.log('localhost listing 8000!!'));
